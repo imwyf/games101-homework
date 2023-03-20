7dd08071336ab25db775a0faf6c1fc6815362279
imwyf <1185095602@qq.com>
2023-03-20 19:34:27 +0800

    MSAA -> ok

--- GAMES101_Homework2_S2021/Homework2/┤·┬ы┐Є╝▄/rasterizer.cpp
+++ GAMES101_Homework2_S2021/Homework2/┤·┬ы┐Є╝▄/rasterizer.cpp
@@ -10,6 +10,15 @@
 #include <opencv2/opencv.hpp>
 #include <math.h>
 
+// 生成采样点
+std::vector<Eigen::Vector3f> genSSList(int x,int y)
+{
+    Eigen::Vector3f a(x,y,1.0f);
+    Eigen::Vector3f b(x,y+0.5,1.0f);
+    Eigen::Vector3f c(x+0.5,y,1.0f);
+    Eigen::Vector3f d(x+0.5,y+0.5,1.0f);
+    return {a,b,c,d};
+}
 
 rst::pos_buf_id rst::rasterizer::load_positions(const std::vector<Eigen::Vector3f> &positions)
 {
@@ -118,9 +127,9 @@ void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf
 }
 
 //Screen space rasterization
-void rst::rasterizer::rasterize_triangle(const Triangle& t) {
+void rst::rasterizer::rasterize_triangle(const Triangle& t) 
+{
     auto v = t.toVector4();
-    // std::cout << t.v[0] <<"\n"<< t.v[1]<<"\n" << t.v[2] << std::endl;
     // get the bounding box of current triangle
     int minX,minY,maxX,maxY;
     minX = std::min(v[0].x(), std::min(v[1].x(), v[2].x()));
@@ -134,29 +143,40 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t) {
         {
             // 扩展作业：MSAA
             // 每个像素内部维护一个数组sample_list，对里面的每个采样点都判断一次在不在三角形内，按照百分比来给予灰度值
-            
-
-            if(insideTriangle(x,y,t.v)) 
+            auto ssList = genSSList(x,y);
+            int judge = 0;
+            // 对每个采样点用z_buffer
+            for (int i=0; i<ssList.size(); i++)
             {
-                auto tup = computeBarycentric2D(x, y, t.v);
-                float alpha;
-                float beta;
-                float gamma;
-                std::tie(alpha, beta, gamma) = tup;
-                // 深度插值
-                float w_reciprocal = 1.0/(alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
-                float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
-                z_interpolated *= w_reciprocal;
-                
-                // 与depth_buf比较
-                if(z_interpolated < depth_buf[get_index(x,y)])
+                if (insideTriangle(ssList[i].x(), ssList[i].y(), t.v))
                 {
-                    set_pixel(Vector3f(x,y,z_interpolated),t.getColor());
-                    depth_buf[get_index(x,y)] = z_interpolated;
+                    auto tup = computeBarycentric2D(ssList[i].x(), ssList[i].y(), t.v);
+                    float alpha;
+                    float beta;
+                    float gamma;
+                    std::tie(alpha, beta, gamma) = tup;
+                    float w_reciprocal = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
+                    float z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
+                    z_interpolated *= w_reciprocal;
+                    if (super_depth_buf[get_super_index(x*2 + i % 2, y*2 + i / 2)] > z_interpolated)
+                    {
+                        judge = 1;
+                        //深度存入缓存
+                        super_depth_buf[get_super_index(x*2 + i % 2, y*2 + i / 2)] = z_interpolated;
+                        //颜色存入缓存
+                        super_frame_buf[get_super_index(x*2 + i % 2, y*2 + i / 2)] = t.getColor();
+                    }
                 }
             }
+            if (judge)
+            //若像素的四个样本中有一个通过了深度测试，就需要对该像素进行着色，因为有一个通过就说明有颜色，就需要着色。
+            {
+                Vector3f point = {x,y,0 };
+                Vector3f color = (super_frame_buf[get_super_index(x*2 , y*2)]+ super_frame_buf[get_super_index(x*2+1, y*2)]+ super_frame_buf[get_super_index(x*2, y*2+1)]+ super_frame_buf[get_super_index(x*2+1, y*2+1)])/4;
+                //着色
+                set_pixel(point, color);
+            }
         }
-    
 }
 
 void rst::rasterizer::set_model(const Eigen::Matrix4f& m)
@@ -179,10 +199,12 @@ void rst::rasterizer::clear(rst::Buffers buff)
     if ((buff & rst::Buffers::Color) == rst::Buffers::Color)
     {
         std::fill(frame_buf.begin(), frame_buf.end(), Eigen::Vector3f{0, 0, 0});
+        std::fill(super_frame_buf.begin(), super_frame_buf.end(), Eigen::Vector3f{ 0, 0, 0 });
     }
     if ((buff & rst::Buffers::Depth) == rst::Buffers::Depth)
     {
         std::fill(depth_buf.begin(), depth_buf.end(), std::numeric_limits<float>::infinity());
+        std::fill(super_depth_buf.begin(), super_depth_buf.end(), std::numeric_limits<float>::infinity());
     }
 }
 
@@ -190,6 +212,8 @@ rst::rasterizer::rasterizer(int w, int h) : width(w), height(h)
 {
     frame_buf.resize(w * h);
     depth_buf.resize(w * h);
+    super_frame_buf.resize(w * h * 4);
+    super_depth_buf.resize(w * h * 4);
 }
 
 int rst::rasterizer::get_index(int x, int y)
@@ -197,12 +221,16 @@ int rst::rasterizer::get_index(int x, int y)
     return (height-1-y)*width + x;
 }
 
-void rst::rasterizer::set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color)
+int rst::rasterizer::get_super_index(int x, int y)
+{
+    return (height*2 - 1 - y) * width*2 + x;
+}
+
+void rst::rasterizer::set_pixel(const Eigen::Vector3f& point,const Eigen::Vector3f& color)
 {
     //old index: auto ind = point.y() + point.x() * width;
     auto ind = (height-1-point.y())*width + point.x();
     frame_buf[ind] = color;
 
 }
-
 // clang-format on
--- GAMES101_Homework2_S2021/Homework2/┤·┬ы┐Є╝▄/rasterizer.hpp
+++ GAMES101_Homework2_S2021/Homework2/┤·┬ы┐Є╝▄/rasterizer.hpp
@@ -58,26 +58,26 @@ namespace rst
     {
     public:
         rasterizer(int w, int h);
-        pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
-        ind_buf_id load_indices(const std::vector<Eigen::Vector3i>& indices);
-        col_buf_id load_colors(const std::vector<Eigen::Vector3f>& colors);
+        pos_buf_id load_positions(const std::vector<Eigen::Vector3f> &positions);
+        ind_buf_id load_indices(const std::vector<Eigen::Vector3i> &indices);
+        col_buf_id load_colors(const std::vector<Eigen::Vector3f> &colors);
 
-        void set_model(const Eigen::Matrix4f& m);
-        void set_view(const Eigen::Matrix4f& v);
-        void set_projection(const Eigen::Matrix4f& p);
+        void set_model(const Eigen::Matrix4f &m);
+        void set_view(const Eigen::Matrix4f &v);
+        void set_projection(const Eigen::Matrix4f &p);
 
-        void set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color);
+        void set_pixel(const Eigen::Vector3f &point, const Eigen::Vector3f &color);
 
         void clear(Buffers buff);
 
         void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type);
 
-        std::vector<Eigen::Vector3f>& frame_buffer() { return frame_buf; }
+        std::vector<Eigen::Vector3f> &frame_buffer() { return frame_buf; }
 
     private:
         void draw_line(Eigen::Vector3f begin, Eigen::Vector3f end);
 
-        void rasterize_triangle(const Triangle& t);
+        void rasterize_triangle(const Triangle &t);
 
         // VERTEX SHADER -> MVP -> Clipping -> /.W -> VIEWPORT -> DRAWLINE/DRAWTRI -> FRAGSHADER
 
@@ -91,11 +91,13 @@ namespace rst
         std::map<int, std::vector<Eigen::Vector3f>> col_buf;
 
         std::vector<Eigen::Vector3f> frame_buf;
-
         std::vector<float> depth_buf;
-        int get_index(int x, int y);
+        std::vector<Eigen::Vector3f> super_frame_buf;
+        std::vector<float> super_depth_buf;
 
         int width, height;
+        int get_index(int x, int y);
+        int get_super_index(int x, int y);
 
         int next_id = 0;
         int get_next_id() { return next_id++; }
